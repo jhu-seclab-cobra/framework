5957794b01519024acd1a58171276bb79283a5d3
SamHsu <yxu166@jh.edu>
2026-03-24 12:50:54 -0400

    fix: Update dependency substitutions for commons-value and commons-graph in settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,6 @@ plugins {
 }
 
 rootProject.name = "framework"
-include("framework")
 
 // Only include transitive dependencies when building standalone (not as a composite build).
 // When included by a parent project, the parent is responsible for providing commons-value and commons-graph.
@@ -11,13 +10,16 @@ if (gradle.parent == null && file("extern/commons-graph").exists()) {
     includeBuild("extern/commons-graph/extern/commons-value") {
         name = "commons-value"
         dependencySubstitution {
-            substitute(module("com.github.jhu-seclab-cobra:commons-value")).using(project(":lib"))
+            substitute(module("com.github.jhu-seclab-cobra:commons-value")).using(project(":jhu-seclab-cobra-commons-value"))
         }
     }
     includeBuild("extern/commons-graph") {
         name = "commons-graph"
         dependencySubstitution {
-            substitute(module("com.github.jhu-seclab-cobra.commons-graph:graph")).using(project(":graph"))
+            substitute(module("com.github.jhu-seclab-cobra.commons-graph:graph")).using(project(":jhu-seclab-cobra-commons-graph"))
         }
     }
 }
+
+include("jhu-seclab-cobra-framework")
+project(":jhu-seclab-cobra-framework").projectDir = file("framework")
